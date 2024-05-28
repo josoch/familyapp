da65e378be42582f8986ffd76caf2d0447a49f91
Josoch <ocholijoseph@gmail.com>
2024-05-28 10:29:23 +0000

    Coding the Income head

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,71 +1,27 @@
 <template>
-  <div>
-    <h1>CRUD Operations</h1>
-    <form @submit.prevent="createItem">
-      <input type="text" v-model="newItem.name" />
-      <button type="submit">Create</button>
-    </form>
-    <ul>
-      <li v-for="item in items" :key="item.id">
-        {{ item.name }}
-        <button @click="editItem(item)">Edit</button>
-        <button @click="deleteItem(item)">Delete</button>
-      </li>
-    </ul>
-    <form @submit.prevent="updateItem" v-if="editing">
-      <input type="text" v-model="editing.name" />
-      <button type="submit">Update</button>
-    </form>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        <h4>
+          Income
+          <RouterLink to="income/create" class="btn btn-primary">Add Income</RouterLink>
+        </h4>
+      </div>
+      <div class="card-body">
+        <table class-"table table-bordered>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Date</th>
+              <th>Manager</th>
+              <th>Currency</th>
+              <th>Type</th>
+              <th>Transaction</th>
+              <th>Amount</th>
+            </tr>
+          </thead>       
+        </table>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-import api from '../api'
-
-export default {
-  data() {
-    return {
-      items: [],
-      newItem: { name: '' },
-      editing: null
-    }
-  },
-  mounted() {
-    this.readItems()
-  },
-  methods: {
-    createItem() {
-      api.post('/items', this.newItem)
-        .then(response => {
-          this.items.push(response.data)
-          this.newItem = { name: '' }
-        })
-        .catch(error => console.error(error))
-    },
-    readItems() {
-      api.get('/items')
-        .then(response => {
-          this.items = response.data
-        })
-        .catch(error => console.error(error))
-    },
-    editItem(item) {
-      this.editing = item
-    },
-    updateItem() {
-      api.put(`/items/${this.editing.id}`, this.editing)
-        .then(response => {
-          this.editing = null
-        })
-        .catch(error => console.error(error))
-    },
-    deleteItem(item) {
-      api.delete(`/items/${item.id}`)
-        .then(response => {
-          this.items = this.items.filter(i => i.id !== item.id)
-        })
-        .catch(error => console.error(error))
-    }
-  }
-}
-</script>
